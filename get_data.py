7b49ead6010cbf280249224f86cba9987cb20078
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2017-02-01 11:40:44 +0900

    added count functions to get_data.py, and counted

--- get_data.py
+++ get_data.py
@@ -2,6 +2,7 @@
 
 import re
 import os
+from collections import Counter
 
 sources_directory = os.path.expanduser("~/Dropbox/programs/tex/arxiv_sources")
 
@@ -15,7 +16,45 @@ def get_subject_area(article_id):
     subject = match_obj.group(2)
     return field, subject
 
+def get_packages(article_id):
+    article_dir = sources_directory + "/" + article_id
+    files_of_article = os.listdir(article_dir)
+    package_list = []
+    for texfile_name in filter(lambda s: re.search(r"\.tex$", s), files_of_article):
+        f = open(article_dir + "/" + texfile_name)
+        tex_code = f.read()
+        f.close()
+        pattern = r"\\usepackage\{([a-zA-Z0-9\-,]*)\}"
+        for match_obj in re.finditer(pattern, tex_code):
+            package_list = package_list + match_obj.group(1).split(",")
+    return package_list
+
+def count_packages(article_id_list, field_list=None):
+    # field_list=None => all fields
+    # field_list=["math", "cs"] => count only in math and cs
+    package_list = []
+    for article_id in article_id_list:
+        if (field_list is None) or (get_subject_area(article_id)[0] in field_list):
+            package_list = package_list + get_packages(article_id)
+    counter = Counter(package_list)
+    return counter.most_common()
+
+def count_areas(article_id_list):
+    area_list = []
+    for article_id in article_id_list:
+        area_list.append(get_subject_area(article_id)[0])
+    counter = Counter(area_list)
+    return counter.most_common()
+
 if __name__ == '__main__':
-    for i in range(1, 501):
-        article_id = "1701.%05d" % i
-        print article_id, get_subject_area(article_id)
+    max_id = 1000
+    # for i in range(1, max_id+1):
+    #     article_id = "1701.%05d" % i
+    #     print article_id, get_subject_area(article_id), get_packages(article_id)
+    article_id_list = map(lambda i: "1701.%05d" % i, list(range(1,max_id+1)))
+    #print count_packages(article_id_list, field_list=None)
+    for package, count in count_packages(article_id_list, field_list=["math"]):
+        print package, count
+    print "-------------"
+    for area, count in count_areas(article_id_list):
+        print area, count
--- repeat-download-arxiv.sh
+++ repeat-download-arxiv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for i in $(seq -f %05g 101 500); do
+for i in $(seq -f %05g 501 1000); do
     echo "downloading 1701.$i..."
     bash download-arxiv.sh 1701.$i > /dev/null 2>&1 &
-    sleep 5s
+    sleep 1s
 done
