9cc86b7c7f160c820ac63a29cd35b30323d7c0ee
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2017-09-29 23:39:19 +0900

    add exclude_pattern and modify

--- get_data.py
+++ get_data.py
@@ -29,11 +29,19 @@ def get_data_from_texfile(article_id, pattern):
     return data_list
 
 
-def count_data(article_id_list, pattern, field_list=None):
+def count_data(article_id_list, pattern, field_list=None, exclude_pattern_list=None):
     data_list = []
     for article_id in article_id_list:
         if (field_list is None) or (get_subject_area(article_id)[0] in field_list):
-            data_list = data_list + get_data_from_texfile(article_id, pattern)
+            data_list_temp = get_data_from_texfile(article_id, pattern)
+            if exclude_pattern_list is not None:
+                for exclude_pattern in exclude_pattern_list:
+                    data_to_exclude = get_data_from_texfile(article_id, exclude_pattern)
+                    for exc in data_to_exclude:
+                        data_list_temp = filter(lambda d: d!=exc, data_list_temp)
+            if "prop" in data_list_temp:
+                print article_id
+            data_list = data_list + data_list_temp
     counter = Counter(data_list)
     return counter.most_common()
 
@@ -44,43 +52,38 @@ def count_areas(article_id_list):
     counter = Counter(area_list)
     return counter.most_common()
 
-if __name__ == '__main__':
+def print_count(article_id_list, pattern, field_list=None, exclude_pattern_list=None, name=""):
+    total_count = 0
+    print "\n-----%s-----" % name
+    output = ""
+    for data, count in count_data(article_id_list, pattern,
+                                  field_list=field_list, exclude_pattern_list=exclude_pattern_list):
+        # print data, count
+        output += "%s(%s) " % (data, str(count))
+        total_count += count
+    print "total_count: %d" % total_count
+    print output
+
+def main():
     package_regexp       = r"^[^%\n]*\\usepackage(?:\[[^\]]*\])?\{([a-zA-Z0-9\-,]*)\}"
     documentclass_regexp = r"^[^%\n]*\\documentclass(?:\[[^\]]*\])?\{([a-zA-Z0-9\-,]*)\}"
     environment_regexp   = r"\\end\{([a-zA-Z0-9\-,*]*)\}"
-    newtheorem_regexp    = r"\\newtheorem{([a-zA-Z]*)}(?:\[[a-zA-Z]*\])?\{([^}\n]*)\}"
+    newtheorem_regexp    = r"\\newtheorem\*? *\{([a-zA-Z0-9*]*)\} *(?:\[[a-zA-Z0-9*]*\])? *\{([^}\n]*)\}"
+    newenvironment_regexp = r"\\newenvironment *\{([a-zA-Z0-9*]*)\}"
 
     max_id = 1000
     article_id_list = map(lambda i: "1701.%05d" % i, list(range(1,max_id+1)))
     field_list = None
     field_list = ["math"]
 
-    total_count = 0
-    print "---package---"
-    for package, count in count_data(article_id_list, package_regexp, field_list=field_list):
-        print package, count
-        total_count += count
-    print "total_count: %d" % total_count
-    print "-------------"
+    print_count(article_id_list, documentclass_regexp, field_list, name="documentclass")
+    print_count(article_id_list, package_regexp, field_list, name="package")
+    print_count(article_id_list, environment_regexp, field_list,
+                exclude_pattern_list=[newenvironment_regexp, newtheorem_regexp], name="environment")
 
-    total_count = 0
-    print "---environment---"
-    for environment, count in count_data(article_id_list, environment_regexp, field_list=field_list):
-        print environment, count
-        total_count += count
-    print "total_count: %d" % total_count
-    print "-------------"
-    print "---theorem---"
-    for theorem, count in count_data(article_id_list, newtheorem_regexp, field_list=field_list):
-        print theorem, count
-        total_count += count
-    print "total_count: %d" % total_count
-    print "-------------"
-
-    print "---documentclass---"
-    for documentclass, count in count_data(article_id_list, documentclass_regexp, field_list=field_list):
-        print documentclass, count
-    print "-------------"
-
+    print "\n-----area-----"
     for area, count in count_areas(article_id_list):
         print area, count
+
+if __name__ == '__main__':
+    main()
